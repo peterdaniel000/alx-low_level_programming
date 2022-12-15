c4911c8600806aa50cf1ff34d6ad484912fc6b16
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-15 00:32:49 -0800

    learning c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
  * return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts( "Programming is like building a multilingual puzzle" );
 	return (0);
-}
+}	
