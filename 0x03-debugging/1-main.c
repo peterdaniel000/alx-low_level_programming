851014632c2d3537234e59ef22c0df7b111af1c6
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-16 21:07:05 -0800

    learning c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - infinite loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int 1;
+
+printf("infinite loop incoming :\n");
+
+i = 0;
+
+/*
+ * while (i < 10)
+ * {
+ * infinite loop - no increase in variables
+ * putchar(i);
+ }
+ */
+printf("infinite loop avoided! :\n");
+return (0);
+}
