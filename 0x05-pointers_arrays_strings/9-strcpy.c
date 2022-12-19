d634f34682e804538080ab0698d3ad2c7af53398
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 12:56:28 -0800

    learning c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcpy - copy
+ * @dest: char parameter
+ * @src: char parameter
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
