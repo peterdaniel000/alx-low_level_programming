4585ee412cc646def3a1fafa615563c2dec3fe8c
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 09:35:25 -0800

    learning c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * rev_string - reverse
+ * @s: char parameter
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+int n = strlen(s);
+
+int i;
+for (i = 0; i < n / 2; i++;)
+{
+char ch = s[i];
+s[i] = s[n - i - 1];
+s[n - i - 1] = ch
+}
+}
