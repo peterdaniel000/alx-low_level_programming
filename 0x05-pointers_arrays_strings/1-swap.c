30480bff83038b19a2a2904d9f63872983ad9e83
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 07:29:21 -0800

    swaping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap
+ * @a: int parameter
+ * @b: int parameter
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+temp = *b;
+}
