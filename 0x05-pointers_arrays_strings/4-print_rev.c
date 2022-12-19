25c6a197c4e572ff696ff2e0778fe4f291a41f59
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 09:02:51 -0800

    learning c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * print_rev - reverse
+ * @s - char parameter
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (i = i - 1; i >= o; i--)
+{
+_putchar(s[i]);
+}
+_putchar('s\n');
+}
