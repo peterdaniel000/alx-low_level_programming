b69e2251aeea9c487546e7b5d6b3d1bb28347a12
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 08:03:55 -0800

    learning string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * int_strlen - lenght
+ * @s: int parameter
+ * Return: int of s
+ */
+
+int_strlen(char *s)
+{
+int num;
+
+num = strlen(s);
+return (num);
+}
