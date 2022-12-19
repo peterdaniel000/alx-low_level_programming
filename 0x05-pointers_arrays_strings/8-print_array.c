cea7a395d5adb70e95acc41ea6b4d8d3b181c368
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-19 12:32:17 -0800

    learning c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 
 /**
  * print_array - printing n of an array
- * @a: int parameter
- * @n: int parameter
- * Return: Nothing
+ * @a: thats our array
+ * @n: this is the #
+ * Return: void
  */
 void print_array(int *a, int n)
 {
@@ -16,7 +16,7 @@ printf("%d", a[i]);
 i++;
 n--;
 }
-for (n > 0; n--);
+for (n > 0; n--)
 {
 printf("%d", a[i]);
 i++;
