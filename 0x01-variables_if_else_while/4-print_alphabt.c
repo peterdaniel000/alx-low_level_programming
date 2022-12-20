4bcf36e72639cf50753fc0b1d753c5fe55eb9676
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-20 10:57:16 -0800

    learning c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 after function run
+ */
+
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch == 'e' || ch == 'q')
+continue;
+putchar(ch);
+printf("\n");
+return (0);
+}
