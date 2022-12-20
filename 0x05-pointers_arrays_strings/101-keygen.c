678238470c3433f46c077de462aa8da8a8ab2011
peterdaniel000 <118739591+peterdaniel000@users.noreply.github.com>
2022-12-20 05:51:23 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -32,25 +32,25 @@ for (i = 0; i < N; i++)
 if (randomizer == 1)
 {
 password[i] = number[rand() % 10];
-randomizer = rand() % 4;
+randomizer = int (*)(void);
 printf("%c", password[i]);
 }
 else if (randomizer == 2)
 {
 password[i] = symbol[rand() % 8];
-randomizer = rand() % 4;
+randomizer = int (*)(void);
 printf("%c", password[i]);
 }
 else if (randomizer == 3)
 {
 password[i] = LETTER[rand() % 26];
-randomizer = rand % 4;
+randomizer = int (*)(void);
 printf("%c", password[i]);
 }
 else
 {
 password[i] = letter[rand() % 26];
-randomizer = rand % 4;
+randomizer = int (*)(void)
 printf("%c", password[i]);
 }
 }
