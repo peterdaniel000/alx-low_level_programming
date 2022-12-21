7a1562d39a8eb03c14f9d9b69310ebd2b103d8a9
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-21 02:40:14 -0800

    if...else

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 after run function
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch < 'a'; ch++)
+putchar(ch);
+printf("\n");
+return (0);
+}
