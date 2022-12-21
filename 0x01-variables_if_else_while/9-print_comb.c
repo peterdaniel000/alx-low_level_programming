6cef0a4055b6c0775515d604950ae869f9e34936
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-21 03:17:53 -0800

    if...else

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 after run function
+ */
+int main(void)
+{
+int x;
+
+for (x = 48; x < 58; x++)
+putchar(x);
+if (x < 57)
+{
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
