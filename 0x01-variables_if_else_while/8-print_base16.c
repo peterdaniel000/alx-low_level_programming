b6c71f0f43d8a410b90ff7a0eebde58a89e13f2f
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-21 03:01:01 -0800

    if...else

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 after function run
+ */
+int main(void)
+{
+int d;
+for (d = 1; d < 16; d++)
+printf("%x", d);
+printf("\n");
+return (0);
+}
