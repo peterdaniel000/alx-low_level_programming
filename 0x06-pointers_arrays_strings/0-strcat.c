f25d49f0cdc5eff1503981c535fa27f916b95ddd
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-22 00:33:35 -0800

    concentrate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat - more on it
+ * @dest: char parameter
+ * @src: char
+ * Return: char string
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
