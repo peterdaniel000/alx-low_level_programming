9c91418e3dede5f87946bee872bc718ff0a6f8d7
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-22 03:11:57 -0800

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * string_toupper - function to capitalize letters in string
+ * @s: that's the string
+ *
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+char *p = s;
+
+for (; s; s++)
+{
+if (*s > 96 && *s < 123)
+{
+*s = *s - 32;
+}
+}
+return (p);
+}
