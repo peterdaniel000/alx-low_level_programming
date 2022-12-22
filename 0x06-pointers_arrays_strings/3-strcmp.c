43809a74c76dc7cb382dc57f3dd562b422efb2f6
peterdaniel000 <peterdanielfun24@gmail.com>
2022-12-22 02:19:33 -0800

    comparing string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - more on string
+ * @s1: char parameter
+ * @s2: char parameter
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+int n;
+n = strcmp(s1, s2);
+return (n);
+}
