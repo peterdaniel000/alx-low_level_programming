39112ae21eb0025d5e0f4401cdb56153abe22fbc
peterdaniel000 <peterdanielfun24@gmail.com>
2023-01-11 04:29:16 -0800

    for loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0 after run function
+ */
+
+void print_alphabet(void)
+{
+	char dan = "a";
+
+	for (dan < "a"; dan < "z"; dan++)
+	{
+		_putchar(dan);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 char _putchar(char c);
+void print_alphabet(void);
 
 #endif
