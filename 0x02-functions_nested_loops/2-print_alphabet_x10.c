d6aaf54a7f4875febc0ab9e08f11d3e178447adc
peterdaniel000 <peterdanielfun24@gmail.com>
2023-01-12 00:54:11 -0800

    loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: null
+ */
+
+void print_alphabet_x10(void)
+{
+	char dan = "abcdefghijklmnopqrstuvwxyz";
+
+	for (dan = 0; dan <= 10; dan++);
+	{
+		_putchar(dan);
+	}
+	_putchar('\n');
+}
