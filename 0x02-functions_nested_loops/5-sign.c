7c74abd6a7bdd6c480d64527ac5b51a1786feda6
peterdaniel000 <peterdanielfun24@gmail.com>
2023-01-12 04:49:38 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ *
+ * @n: int parameter
+ * Return: int
+ */
+
+int print_sign(int n)
+{
+	int i;
+
+	for (i = 0; i <= 135; i++)
+	{
+		if (i > n)
+		{
+			return (1);
+		}
+		else if (i < n)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
